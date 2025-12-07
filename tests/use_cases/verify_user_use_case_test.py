c797f9c4327d897916f48496b76f444ac40607fb
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-12-07 07:25:07 +0100

    feat[test/use_cases]: test verify user, not bearer token, invalid token, wrong type, expired token

---
+++ tests/use_cases/verify_user_use_case_test.py
@@ -0,0 +1,95 @@
+from uuid import uuid4
+import pytest
+from app.core.config import settings
+from app.core.security import jwt_service
+from app.domain.users.use_cases.DTO.login_dto import VerifyInput
+from app.domain.users.use_cases.verify_user import VerifyUserUseCase
+
+
+def test_verify_user(repo, make_user):
+    user = make_user()
+    repo.create(user)
+
+    use_case = VerifyUserUseCase(repo)
+    access_token = jwt_service.create(
+        subject=str(user.id),
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES,
+        token_type="access",
+    )
+    input_data = VerifyInput(authorization=f"Bearer {access_token}")
+    result = use_case.execute(input_data)
+
+    assert result.valid is True
+    assert result.sub == str(user.id)
+    assert result.type == "access"
+
+
+def test_not_bearer_token(repo, make_user):
+    user = make_user()
+    repo.create(user)
+
+    use_case = VerifyUserUseCase(repo)
+    access_token = jwt_service.create(
+        subject=str(user.id),
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES,
+        token_type="access",
+    )
+    input_data = VerifyInput(authorization=f"Not a Bearer {access_token}")
+
+    with pytest.raises(ValueError) as exception:
+        use_case.execute(input_data)
+
+    assert str(exception.value) == "missing_bearer_token"
+
+
+def test_invalid_token(repo):
+    use_case = VerifyUserUseCase(repo)
+    fake_id = uuid4()
+    access_token = jwt_service.create(
+        subject=str(fake_id),
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES,
+        token_type="access",
+    )
+    input_data = VerifyInput(authorization=f"Bearer {access_token}")
+
+    with pytest.raises(ValueError) as exception:
+        use_case.execute(input_data)
+
+    assert str(exception.value) == "invalid_token"
+
+
+def test_wrong_token_type(repo, make_user):
+    user = make_user()
+    repo.create(user)
+
+    use_case = VerifyUserUseCase(repo)
+    access_token = jwt_service.create(
+        subject=str(user.id),
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES,
+        token_type="refresh",
+    )
+    input_data = VerifyInput(authorization=f"Bearer {access_token}")
+
+    with pytest.raises(ValueError) as exception:
+        use_case.execute(input_data)
+
+    assert str(exception.value) == "invalid_token"
+
+
+def test_expired_token(repo, make_user):
+    user = make_user()
+    repo.create(user)
+
+    use_case = VerifyUserUseCase(repo)
+    access_token = jwt_service.create(
+        subject=str(user.id),
+        minutes=-1,
+        token_type="access",
+    )
+
+    input_data = VerifyInput(authorization=f"Bearer {access_token}")
+
+    with pytest.raises(ValueError) as ex:
+        use_case.execute(input_data)
+
+    assert str(ex.value) == "invalid_token"
