0ff7943d56c7201c182db8d16fc2288921b128be
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-01-05 11:37:42 +0100

    feat[api/v1/user]: added routes for  create user resend activation email deactivate and activate user  promote and demote user

--- app/api/schemas/user.py
+++ app/api/schemas/user.py
@@ -4,3 +4,7 @@ from pydantic import BaseModel
 class ChangePasswordRequest(BaseModel):
     old_password: str
     new_password: str
+
+
+class ActivateUserRequest(BaseModel):
+    email: str
--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -2,9 +2,15 @@ from uuid import UUID
 from fastapi import APIRouter, Depends, HTTPException, Query, Response, status
 
 from app.api.dependencies.auth import get_current_active_user, get_current_admin_user
+from app.api.dependencies.notifications import get_email_service
+from app.api.dependencies.security import get_activation_token_service
 from app.api.dependencies.users import get_users_repository
-from app.api.schemas.user import ChangePasswordRequest
+from app.api.schemas.user import ActivateUserRequest, ChangePasswordRequest
 
+from app.domain.notifications.handlers.send_user_activation_email import (
+    SendUserActivationHandler,
+)
+from app.domain.users.use_cases.DTO.create_user_dto import CreateUserInput
 from app.domain.users.use_cases.DTO.get_users_dto import (
     GetUserInput,
     ListUsersInput,
@@ -14,12 +20,99 @@ from app.domain.users.use_cases.DTO.get_users_dto import (
 )
 from app.domain.users.use_cases.DTO.password_dto import ChangePasswordInput
 from app.domain.users.use_cases.DTO.user_output_dto import UserOutput
+from app.domain.users.use_cases.DTO.user_status_dto import (
+    ActivateUserInput,
+    DeactivateUserInput,
+    DemoteUserInput,
+    PromoteUserInput,
+)
+from app.domain.users.use_cases.activate_user import ActivateUserUseCase
 from app.domain.users.use_cases.change_password import ChangePasswordUseCase
+from app.domain.users.use_cases.create_user import CreateUserUseCase
+from app.domain.users.use_cases.deactivate_user import DeactivateUserUseCase
+from app.domain.users.use_cases.demote_user_from_admin import DemoteUserFromAdminUseCase
 from app.domain.users.use_cases.get_user import GetUserUseCase
 from app.domain.users.use_cases.list_users import ListUsersUseCase
+from app.domain.users.use_cases.promote_user_to_admin import PromoteUserToAdminUseCase
+from app.domain.users.use_cases.resend_activation_email import (
+    ResendActivationEmailUseCase,
+)
 
 
 router = APIRouter()
+
+
+@router.post("/users")
+async def create_user(
+    data: ActivateUserRequest,
+    current_user=Depends(get_current_admin_user),
+    repo=Depends(get_users_repository),
+    email_service=Depends(get_email_service),
+    token_service=Depends(get_activation_token_service),
+):
+    try:
+        use_case = CreateUserUseCase(repo)
+        dto = CreateUserInput(user_email=data.email)
+        event = use_case.execute(dto)
+
+        handler = SendUserActivationHandler(
+            email_service=email_service, token_service=token_service
+        )
+        await handler.handle(event)
+    except ValueError as exception:
+        if str(exception) == "user_already_exists":
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT, detail=str(exception)
+            )
+        if str(exception) == "email_service_unavailable":
+            raise HTTPException(
+                status_code=status.HTTP_502_BAD_GATEWAY, detail=str(exception)
+            )
+        if str(exception) == "email_send_failed":
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(exception)
+            )
+
+    return {"message": "User created and activation mail sent"}
+
+
+# TODO: atualizar o create userInput para activateUserInput
+@router.post("/users/resend-email")
+async def resend_email(
+    data: ActivateUserRequest,
+    repo=Depends(get_users_repository),
+    email_service=Depends(get_email_service),
+    token_service=Depends(get_activation_token_service),
+):
+    try:
+        use_case = ResendActivationEmailUseCase(repo)
+        dto = CreateUserInput(user_email=data.email)
+        event = use_case.execute(dto)
+
+        handler = SendUserActivationHandler(
+            email_service=email_service, token_service=token_service
+        )
+        await handler.handle(event)
+
+    except ValueError as exception:
+        if str(exception) == "user_not_found":
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=str(exception)
+            )
+        if str(exception) == "user_has_been_activated_before":
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT, detail=str(exception)
+            )
+        if str(exception) == "email_service_unavailable":
+            raise HTTPException(
+                status_code=status.HTTP_502_BAD_GATEWAY, detail=str(exception)
+            )
+        if str(exception) == "email_send_failed":
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(exception)
+            )
+
+    return {"message": "User created and activation mail sent"}
 
 
 @router.post("/me/change-password")
@@ -81,6 +174,84 @@ def get_user(
 
     use_case = GetUserUseCase(repo)
     dto = GetUserInput(user_id=user_id)
+
+    try:
+        return use_case.execute(dto)
+    except ValueError as exception:
+        if str(exception) == "user_not_found":
+            raise HTTPException(status_code=404, detail="user_not_found")
+        raise
+
+
+@router.patch("/users/deactivate/{user_id}", status_code=204)
+def deactivate_user(
+    user_id: UUID,
+    current_user=Depends(get_current_admin_user),
+    repo=Depends(get_users_repository),
+):
+    use_case = DeactivateUserUseCase(repo)
+    dto = DeactivateUserInput(user_id=user_id, actor_id=current_user.id)
+
+    try:
+        return use_case.execute(dto)
+    except ValueError as exception:
+        if str(exception) == "user_not_found":
+            raise HTTPException(status_code=404, detail="user_not_found")
+        elif str(exception) == "last_admin_cannot_be_deactivated":
+            raise HTTPException(
+                status_code=409, detail="last_admin_cannot_be_deactivated"
+            )
+        elif str(exception) == "cannot_deactivate_yourself":
+            raise HTTPException(status_code=409, detail="cannot_deactivate_yourself")
+        raise
+
+
+@router.patch("/users/activate/{user_id}", status_code=204)
+def activate_user(
+    user_id: UUID,
+    current_user=Depends(get_current_admin_user),
+    repo=Depends(get_users_repository),
+):
+    use_case = ActivateUserUseCase(repo)
+    dto = ActivateUserInput(user_id=user_id)
+
+    try:
+        return use_case.execute(dto)
+    except ValueError as exception:
+        if str(exception) == "user_not_found":
+            raise HTTPException(status_code=404, detail="user_not_found")
+        raise
+
+
+@router.patch("/users/demote/{user_id}", status_code=204)
+def demote_user(
+    user_id: UUID,
+    current_user=Depends(get_current_admin_user),
+    repo=Depends(get_users_repository),
+):
+    use_case = DemoteUserFromAdminUseCase(repo)
+    dto = DemoteUserInput(user_id=user_id, actor_id=current_user.id)
+
+    try:
+        return use_case.execute(dto)
+    except ValueError as exception:
+        if str(exception) == "user_not_found":
+            raise HTTPException(status_code=404, detail="user_not_found")
+        elif str(exception) == "last_admin_cannot_be_demoted":
+            raise HTTPException(status_code=409, detail="last_admin_cannot_be_demoted")
+        elif str(exception) == "cannot_demote_yourself":
+            raise HTTPException(status_code=409, detail="cannot_demote_yourself")
+        raise
+
+
+@router.patch("/users/promote/{user_id}", status_code=204)
+def promote_user(
+    user_id: UUID,
+    current_user=Depends(get_current_admin_user),
+    repo=Depends(get_users_repository),
+):
+    use_case = PromoteUserToAdminUseCase(repo)
+    dto = PromoteUserInput(user_id=user_id)
 
     try:
         return use_case.execute(dto)
