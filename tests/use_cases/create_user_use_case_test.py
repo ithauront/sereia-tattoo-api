091b2c36a25ef0c9a5b5fd23f8449dcf3ed4d17e
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-01-07 10:25:50 +0100

    feat[tests/use_cases]: testing create user, create user that already exists and find by email called befor create a user

---
+++ tests/use_cases/create_user_use_case_test.py
@@ -0,0 +1,49 @@
+from uuid import UUID
+import pytest
+from app.domain.users.events.user_creation_requested import UserCreationRequested
+from app.domain.users.use_cases.DTO.create_user_dto import CreateUserInput
+from app.domain.users.use_cases.create_user import CreateUserUseCase
+
+
+def test_create_user_success(repo):
+    use_case = CreateUserUseCase(repo)
+    input_data = CreateUserInput(user_email="jhon@doe.com")
+
+    result = use_case.execute(input_data)
+
+    saved_user = repo.find_by_email("jhon@doe.com")
+    assert saved_user is not None
+    assert saved_user.email == "jhon@doe.com"
+    assert saved_user.has_activated_once is False
+
+    assert result is not None
+    assert isinstance(result, UserCreationRequested)
+    assert result.email == "jhon@doe.com"
+    assert type(result.user_id) is UUID
+    assert type(result.activation_token_version) is int
+    assert result.activation_token_version == 0
+    assert saved_user.id == result.user_id
+
+
+def test_user_already_exists(repo, make_user, mocker):
+    user = make_user(email="jhon@doe.com")
+    repo.create(user)
+
+    spy = mocker.spy(repo, "create")
+    use_case = CreateUserUseCase(repo)
+    input_data = CreateUserInput(user_email="jhon@doe.com")
+
+    with pytest.raises(ValueError) as exception:
+        use_case.execute(input_data)
+
+    assert str(exception.value) == "user_already_exists"
+    spy.assert_not_called()
+
+
+def test_find_by_email_called_before_create(mocker, repo):
+    spy = mocker.spy(repo, "find_by_email")
+
+    use_case = CreateUserUseCase(repo)
+    use_case.execute(CreateUserInput(user_email="jhon@doe.com"))
+
+    spy.assert_called_once_with("jhon@doe.com")
