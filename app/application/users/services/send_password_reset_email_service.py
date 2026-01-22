fe79ec577c8b112adfa0a475f23a3730908db44c
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-01-22 12:18:04 +0100

    feat[application/users/Services}: Service to send email and bump password token

---
+++ app/application/users/services/send_password_reset_email_service.py
@@ -0,0 +1,43 @@
+from app.domain.notifications.handlers.send_password_reset_email import (
+    SendPasswordResetEmailHandler,
+)
+
+from app.domain.users.events.password_reset_email_requested import (
+    PasswordResetEmailRequested,
+)
+from app.domain.users.repositories.users_repository import UsersRepository
+
+
+from app.domain.users.use_cases.DTO.prepare_send_forgot_password_email_dto import (
+    PrepareSendForgotPasswordEmailInput,
+)
+from app.domain.users.use_cases.prepare_send_forgot_password_email import (
+    PrepareSendForgotPasswordEmailUseCase,
+)
+
+
+# TODO: fazer teste disso
+class SendPasswordResetEmailService:
+    def __init__(
+        self,
+        repo: UsersRepository,
+        prepare_use_case: PrepareSendForgotPasswordEmailUseCase,
+        email_handler: SendPasswordResetEmailHandler,
+    ):
+        self.repo = repo
+        self.prepare_use_case = prepare_use_case
+        self.email_handler = email_handler
+
+    async def execute(self, data: PrepareSendForgotPasswordEmailInput) -> None:
+        user = self.prepare_use_case.execute(data)
+
+        event = PasswordResetEmailRequested(
+            user_id=user.id,
+            email=user.email,
+            password_token_version=user.password_token_version + 1,
+        )
+
+        await self.email_handler.handle(event)
+
+        user.bump_password_token()
+        self.repo.update(user)
