05f483aea6b2e3b5ef1dda5a7bb96b97e6a76821
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-01-28 11:10:12 +0100

    feat[tests/api/users]: test request password reset request route

---
+++ tests/api/users/request_password_reset_route_test.py
@@ -0,0 +1,139 @@
+from fastapi.testclient import TestClient
+from app.main import app
+from app.api.dependencies.users import get_users_repository
+from tests.fakes.fake_email_service import FakeEmailService
+from app.api.dependencies.notifications import get_email_service
+
+
+client = TestClient(app)
+
+
+def test_request_reset_password_success(repo, make_user):
+    user = make_user(email="jhon@doe.com", password_token_version=0)
+    repo.create(user)
+
+    payload = {"email": "jhon@doe.com"}
+
+    fake_email_service = FakeEmailService()
+
+    app.dependency_overrides[get_email_service] = lambda: fake_email_service
+    app.dependency_overrides[get_users_repository] = lambda: repo
+
+    response = client.post(
+        "/me/reset-password-request",
+        json=payload,
+    )
+
+    assert response.status_code == 200
+    assert (
+        response.json()["message"]
+        == "if user exists and is active a link was sent to reset password"
+    )
+
+    assert fake_email_service.sent is True
+    assert fake_email_service.last_payload["to"] == "jhon@doe.com"
+    assert fake_email_service.last_payload["subject"] == "Recuperação de senha"
+    assert (
+        "Para criar uma nova senha, clique no botão abaixo:"
+        in fake_email_service.last_payload["html"]
+    )
+    assert user.password_token_version == 1
+
+    app.dependency_overrides.clear()
+
+
+def test_request_reset_password_user_inactive(repo, make_user):
+    user = make_user(email="jhon@doe.com", is_active=False, password_token_version=0)
+    repo.create(user)
+
+    payload = {"email": "jhon@doe.com"}
+
+    fake_email_service = FakeEmailService()
+
+    app.dependency_overrides[get_email_service] = lambda: fake_email_service
+    app.dependency_overrides[get_users_repository] = lambda: repo
+
+    response = client.post(
+        "/me/reset-password-request",
+        json=payload,
+    )
+
+    assert response.status_code == 200
+    assert (
+        response.json()["message"]
+        == "if user exists and is active a link was sent to reset password"
+    )  # Aqui uso uma mensagem generica para não dar info sobre situação de possivel usuario
+
+    assert fake_email_service.sent is False
+    assert user.password_token_version == 0
+
+    app.dependency_overrides.clear()
+
+
+def test_request_reset_password_user_not_found(repo):
+    payload = {"email": "jhon@doe.com"}
+
+    fake_email_service = FakeEmailService()
+
+    app.dependency_overrides[get_email_service] = lambda: fake_email_service
+    app.dependency_overrides[get_users_repository] = lambda: repo
+
+    response = client.post("/me/reset-password-request", json=payload)
+
+    assert response.status_code == 200
+    assert (
+        response.json()["message"]
+        == "if user exists and is active a link was sent to reset password"
+    )  # Aqui uso uma mensagem generica para não dar info sobre situação de possivel usuario
+
+    assert fake_email_service.sent is False
+
+    app.dependency_overrides.clear()
+
+
+def test_request_reset_password_email_service_unavailable(repo, make_user):
+    user = make_user(email="jhon@doe.com", password_token_version=0)
+    repo.create(user)
+
+    payload = {"email": "jhon@doe.com"}
+
+    fake_email_service = FakeEmailService(fail_with="email_service_unavailable")
+
+    app.dependency_overrides[get_email_service] = lambda: fake_email_service
+    app.dependency_overrides[get_users_repository] = lambda: repo
+
+    response = client.post("/me/reset-password-request", json=payload)
+
+    assert response.status_code == 502
+    assert response.json()["detail"] == "email_service_unavailable"
+    assert user.password_token_version == 0
+    assert fake_email_service.sent is False
+
+    app.dependency_overrides.clear()
+
+
+def test_request_reset_password_email_send_failed(repo, make_user):
+    user = make_user(email="jhon@doe.com", password_token_version=0)
+    repo.create(user)
+
+    payload = {"email": "jhon@doe.com"}
+
+    fake_email_service = FakeEmailService(fail_with="email_send_failed")
+
+    app.dependency_overrides[get_email_service] = lambda: fake_email_service
+    app.dependency_overrides[get_users_repository] = lambda: repo
+
+    response = client.post("/me/reset-password-request", json=payload)
+
+    assert response.status_code == 500
+    assert response.json()["detail"] == "email_send_failed"
+    assert user.password_token_version == 0
+    assert fake_email_service.sent is False
+
+    app.dependency_overrides.clear()
+
+
+def test_request_reset_password_invalid_payload():
+    response = client.post("/me/reset-password-request", json={})
+
+    assert response.status_code == 422
