2f4899e347397e60c60a3b51b3a98efbe91b1330
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-02-12 09:24:55 +0100

    feat[api/vi/auth]: added logout route and remove verify route
    the getuser dependency already does what verify route did so we do not need the verify route
    also now we deal with access and refresh token version on every route

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,23 +1,38 @@
-from fastapi import APIRouter, Depends, HTTPException, status, Header
+from fastapi import APIRouter, Depends, HTTPException, Response, status
+from app.api.dependencies.auth import get_current_user
+from app.api.dependencies.security import (
+    get_access_token_service,
+    get_refresh_token_service,
+)
 from app.api.dependencies.users import get_users_repository
-from app.api.schemas.auth import LoginRequest, RefreshRequest, TokenPair, VerifyResponse
-from app.core.exceptions.users import AuthenticationFailedError, UserInactiveError
+from app.api.schemas.auth import LoginRequest, RefreshRequest, TokenPair
+from app.core.exceptions.security import TokenError
+from app.core.exceptions.users import (
+    AuthenticationFailedError,
+    UserInactiveError,
+)
+from app.core.security.versioned_token_service import VersionedTokenService
 from app.domain.users.use_cases.DTO.login_dto import (
     LoginInput,
+    LogoutInput,
     RefreshInput,
-    VerifyInput,
 )
 from app.domain.users.use_cases.login_user import LoginUserUseCase
+from app.domain.users.use_cases.logout_user import LogoutUserUseCase
 from app.domain.users.use_cases.refresh_user import RefreshUserUseCase
-from app.domain.users.use_cases.verify_user import VerifyUserUseCase
 
 
 router = APIRouter(prefix="/auth")
 
 
 @router.post("/login", response_model=TokenPair)
-def login(data: LoginRequest, repo=Depends(get_users_repository)) -> TokenPair:
-    use_case = LoginUserUseCase(repo)
+def login(
+    data: LoginRequest,
+    repo=Depends(get_users_repository),
+    access_tokens: VersionedTokenService = Depends(get_access_token_service),
+    refresh_tokens: VersionedTokenService = Depends(get_refresh_token_service),
+) -> TokenPair:
+    use_case = LoginUserUseCase(repo, access_tokens, refresh_tokens)
     use_case_input = LoginInput(identifier=data.identifier, password=data.password)
     try:
         result = use_case.execute(use_case_input)
@@ -35,9 +50,34 @@ def login(data: LoginRequest, repo=Depends(get_users_repository)) -> TokenPair:
     )
 
 
+@router.post("/logout")
+def logout_user(
+    current_user=Depends(get_current_user),
+    repo=Depends(get_users_repository),
+):
+    user_id = current_user.id
+    use_case = LogoutUserUseCase(repo)
+
+    dto = LogoutInput(user_id=user_id)
+    use_case.execute(dto)
+    """
+    Apesar do use_case retornar um possivel user_not_found nos não vamos tratar esse erro
+    A dependency ja trata o caso de user_not_found e retorna um 401 token invalid
+    Caso a gente tratasse o user_not_found do use case
+    nos teriamos inconsistencia nos retornos desse erro (401 vs 404)
+    """
+
+    return Response(status_code=204)
+
+
 @router.post("/refresh", response_model=TokenPair)
-def refresh(data: RefreshRequest, repo=Depends(get_users_repository)) -> TokenPair:
-    use_case = RefreshUserUseCase(repo)
+def refresh(
+    data: RefreshRequest,
+    repo=Depends(get_users_repository),
+    access_tokens: VersionedTokenService = Depends(get_access_token_service),
+    refresh_tokens: VersionedTokenService = Depends(get_refresh_token_service),
+) -> TokenPair:
+    use_case = RefreshUserUseCase(repo, refresh_tokens, access_tokens)
     use_case_input = RefreshInput(refresh_token=data.refresh_token)
     try:
         result = use_case.execute(use_case_input)
@@ -45,24 +85,11 @@ def refresh(data: RefreshRequest, repo=Depends(get_users_repository)) -> TokenPa
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid_credentials"
         )
+    except TokenError as exception:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail=exception.message
+        )
 
     return TokenPair(
         access_token=result.access_token, refresh_token=result.refresh_token
     )
-
-
-@router.get("/verify")
-def verify(
-    authorization: str = Header(...), repo=Depends(get_users_repository)
-) -> VerifyResponse:
-    use_case = VerifyUserUseCase(repo)
-    use_case_input = VerifyInput(authorization=authorization)
-
-    try:
-        result = use_case.execute(use_case_input)
-    except AuthenticationFailedError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid_credentials"
-        )
-
-    return VerifyResponse(valid=result.valid, sub=result.sub, token_type=result.type)
