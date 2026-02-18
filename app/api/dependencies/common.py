10fee7ac9b572e32156340580168dd44c5e89102
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-02-18 08:42:36 +0100

    refactor[api/dependencies]: replace the commit that was taken out of repository  in this dependency and added a roolback if something fails

--- app/api/dependencies/common.py
+++ app/api/dependencies/common.py
@@ -2,9 +2,14 @@ from typing import Generator
 from app.infrastructure.sqlalchemy.session import SessionLocal
 
 
+# TODO: talvez criar um getbd read e get db write assim a gente não faz commit nas operações de read e economizamos um pouco de prcessamento
 def get_db() -> Generator:
     db = SessionLocal()
     try:
         yield db
+        db.commit()
+    except Exception:
+        db.rollback()
+        raise
     finally:
         db.close()
