6e2d130471b459bbb11593d144e99206ae545740
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-02-25 10:17:47 +0100

    refactor[application/studio/use_cases/users_use_cases/create_user]: using unit of work in the use case instead of repo. also calling the method to create event of request activation email inside user

--- app/application/studio/use_cases/users_use_cases/create_user.py
+++ app/application/studio/use_cases/users_use_cases/create_user.py
@@ -1,4 +1,4 @@
-from app.application.studio.repositories.users_repository import UsersRepository
+from app.application.studio.unit_of_work.write_unit_of_work import WriteUnitOfWork
 from app.application.studio.use_cases.DTO.create_user_dto import CreateUserInput
 from app.core.exceptions.users import UserAlreadyExistsError
 from app.core.normalize.normalize_email import normalize_email
@@ -9,17 +9,20 @@ from app.domain.studio.users.events.activation_email_requested import (
 
 
 class CreateUserUseCase:
-    def __init__(self, repo: UsersRepository):
-        self.repo = repo
+    def __init__(self, uow: WriteUnitOfWork):
+        self.uow = uow
 
     def execute(self, data: CreateUserInput) -> ActivationEmailRequested:
         email = normalize_email(data.user_email)
-        user_already_exists = self.repo.find_by_email(email)
-        if user_already_exists:
-            raise UserAlreadyExistsError()
 
-        user = User.create_pending(email=email)
-        self.repo.create(user)
-        return ActivationEmailRequested(
-            user_id=user.id, email=user.email, activation_token_version=0
-        )
+        with self.uow:
+            user_already_exists = self.uow.users.find_by_email(email)
+            if user_already_exists:
+                raise UserAlreadyExistsError()
+
+            user = User.create_pending(email=email)
+            self.uow.users.create(user)
+
+            event = user.request_activation_email()
+
+            return event
