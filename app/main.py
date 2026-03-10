e1e63e49f62f4322aa10954ff6b36d4abad2b567
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-03-10 08:52:56 +0100

    feat[main]: added lifespan with event bus state to app

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api import router as api_router
+from app.application.event_bus.setup import setup_event_bus
+from app.core.security import jwt_service
+from app.infrastructure.email.brevo_email_service import BrevoEmailService
 
-app = FastAPI(title="sereia-tattoo-api")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    email_service = BrevoEmailService()
+    token_service = jwt_service
+
+    app.state.event_bus = setup_event_bus(
+        email_service=email_service, token_service=token_service
+    )
+
+    yield
+
+
+app = FastAPI(title="sereia-tattoo-api", lifespan=lifespan)
+
 
 app.add_middleware(
     CORSMiddleware,
