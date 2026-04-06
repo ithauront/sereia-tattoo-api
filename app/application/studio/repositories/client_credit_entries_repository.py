548c012d33622f05514fe3aec022d7c5c989e290
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-04-06 14:07:32 +0200

    feat[apmlication/studio/repositories]: create client credit entries repository
    all the contracts for the client credit entry repository

---
+++ app/application/studio/repositories/client_credit_entries_repository.py
@@ -0,0 +1,62 @@
+from abc import ABC, abstractmethod
+from typing import Optional, List
+from uuid import UUID
+
+from app.application.studio.use_cases.DTO.list_client_credit_entries import Direction
+from app.domain.studio.marketing.entities.client_credit_entry import ClientCreditEntry
+from app.domain.studio.marketing.enums.client_credit_source_type import (
+    ClientCreditSourceType,
+)
+
+
+class ClientCreditEntriesRepository(ABC):
+    @abstractmethod
+    def create(self, client_credit_entry: ClientCreditEntry) -> None: ...
+
+    @abstractmethod
+    def get_balance(self, *, vip_client_id: UUID) -> int: ...
+
+    @abstractmethod
+    def find_by_id(self, credit_id: UUID) -> Optional[ClientCreditEntry]: ...
+
+    @abstractmethod
+    def find_many_by_vip_client_id(
+        self,
+        *,
+        vip_client_id: UUID,
+        limit: int = 100,
+        offset: int = 0,
+        direction: Direction = Direction.desc,
+    ) -> List[ClientCreditEntry]: ...
+
+    @abstractmethod
+    def count_by_vip_client_id(self, *, vip_client_id: UUID) -> int: ...
+
+    @abstractmethod
+    def find_many_by_source_id(
+        self,
+        *,
+        source_id: UUID,
+        limit: int = 100,
+        offset: int = 0,
+        direction: Direction = Direction.desc,
+    ) -> List[ClientCreditEntry]: ...
+
+    @abstractmethod
+    def count_by_source_id(self, *, source_id: UUID) -> int: ...
+
+    @abstractmethod
+    def find_many_by_source_type_and_vip_client_id(
+        self,
+        *,
+        source_type: ClientCreditSourceType,
+        vip_client_id: UUID,
+        limit: int = 100,
+        offset: int = 0,
+        direction: Direction = Direction.desc,
+    ) -> List[ClientCreditEntry]: ...
+
+    @abstractmethod
+    def count_by_source_type_and_vip_client_id(
+        self, *, source_type: ClientCreditSourceType, vip_client_id: UUID
+    ) -> int: ...
