30e574f81094ce710cb5b06c3805c535b58514f0
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-04-08 10:32:23 +0200

    feat[application/studio/unit_of_work]: added client credit entries repo to the base unit of work

--- app/application/studio/unit_of_work/base_unit_of_work.py
+++ app/application/studio/unit_of_work/base_unit_of_work.py
@@ -1,5 +1,8 @@
 from abc import ABC
 
+from app.application.studio.repositories.client_credit_entries_repository import (
+    ClientCreditEntriesRepository,
+)
 from app.application.studio.repositories.users_repository import UsersRepository
 from app.application.studio.repositories.vip_clients_repository import (
     VipClientsRepository,
@@ -9,3 +12,4 @@ from app.application.studio.repositories.vip_clients_repository import (
 class BaseUnitOfWork(ABC):
     users: UsersRepository
     vip_clients: VipClientsRepository
+    client_credit_entries: ClientCreditEntriesRepository
