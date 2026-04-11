b254b442f0ffc1e8f8753094682d9044f5e96097
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-04-11 09:03:13 +0200

    feat[app/api/__init__]: publish routes in init

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.v1 import me, user
+from app.api.v1 import me, user, vip_clients, client_credit_entries
 from .v1 import health
 from .v1 import auth
 
@@ -10,3 +10,5 @@ router.include_router(health.router, tags=["health"])
 router.include_router(auth.router, tags=["auth"])
 router.include_router(user.router, tags=["users"])
 router.include_router(me.router, tags=["me"])
+router.include_router(vip_clients.router, tags=["vip-clients"])
+router.include_router(client_credit_entries.router, tags=["client-credit-entries"])
