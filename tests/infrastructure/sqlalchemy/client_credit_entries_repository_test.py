1cf3b44673080c696c126d80665fea120b9b5fb0
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-04-30 09:38:53 +0200

    feat[tests/infrastructure/sqlalchemy]: client credits repository tests added test to non existent id, not nullables, related entry consistency, empty sums, pagination, over limit offset

--- tests/infrastructure/sqlalchemy/client_credit_entries_repository_test.py
+++ tests/infrastructure/sqlalchemy/client_credit_entries_repository_test.py
@@ -2,7 +2,9 @@ from uuid import uuid4
 
 import pytest
 
-from app.domain.studio.finances.enums.client_credit_source_type import (
+from app.application.studio.use_cases.DTO.commun import Direction
+from app.domain.studio.finances.entities.client_credit_entry import ClientCreditEntry
+from app.core.types.client_credit_source_type import (
     ClientCreditSourceType,
 )
 from app.infrastructure.sqlalchemy.repositories.client_credit_entries_repository import (
@@ -40,17 +42,139 @@ def test_create_and_find_by_id(
     assert found.source_type == "indication"
 
 
+def test_find_by_non_existent_id(
+    sqlalchemy_client_credits_entries_repo: SQLAlchemyClientCreditEntriesRepository,
+):
+    credit_non_existent_id = uuid4()
+    result = sqlalchemy_client_credits_entries_repo.find_by_id(credit_non_existent_id)
+
+    assert result is None
+
+
 def test_cannot_create_without_vip_client(
     sqlalchemy_client_credits_entries_repo: SQLAlchemyClientCreditEntriesRepository,
     make_client_credit_entry,
 ):
 
     entry = make_client_credit_entry(
-        quantity=10,
-        source_type=ClientCreditSourceType.INDICATION,
+        quantity=10, source_type=ClientCreditSourceType.INDICATION, vip_client_id=None
     )
     with pytest.raises(IntegrityError):
         sqlalchemy_client_credits_entries_repo.create(entry)
+
+
+def test_source_id_not_nullable(
+    sqlalchemy_client_credits_entries_repo: SQLAlchemyClientCreditEntriesRepository,
+    sqlalchemy_vip_clients_repo: SQLAlchemyVipClientsRepository,
+    make_vip_client,
+    make_client_credit_entry,
+):
+    vip_client = make_vip_client()
+
+    sqlalchemy_vip_clients_repo.create(vip_client)
+
+    entry = make_client_credit_entry(
+        quantity=10,
+        source_type=ClientCreditSourceType.INDICATION,
+        vip_client_id=vip_client.id,
+        source_id=None,
+    )
+
+    with pytest.raises(IntegrityError) as exc:
+        sqlalchemy_client_credits_entries_repo.create(entry)
+
+    assert "NOT NULL constraint failed" in str(exc.value)
+
+
+def test_related_entry_points_to_another_credit_entry(
+    sqlalchemy_client_credits_entries_repo: SQLAlchemyClientCreditEntriesRepository,
+    sqlalchemy_vip_clients_repo: SQLAlchemyVipClientsRepository,
+    make_vip_client,
+    make_client_credit_entry,
+):
+    vip_client = make_vip_client()
+
+    sqlalchemy_vip_clients_repo.create(vip_client)
+
+    entry1 = make_client_credit_entry(
+        quantity=10,
+        source_type=ClientCreditSourceType.INDICATION,
+        vip_client_id=vip_client.id,
+    )
+    sqlalchemy_client_credits_entries_repo.create(entry1)
+
+    entry2 = make_client_credit_entry(
+        quantity=10,
+        source_type=ClientCreditSourceType.REVERSED_BY_ADMIN,
+        vip_client_id=vip_client.id,
+        related_entry_id=entry1.id,
+    )
+    sqlalchemy_client_credits_entries_repo.create(entry2)
+
+    found = sqlalchemy_client_credits_entries_repo.find_by_id(entry2.id)
+    assert found is not None
+
+    related_entry_id = found.related_entry_id
+    assert related_entry_id is not None
+
+    found_related_entry = sqlalchemy_client_credits_entries_repo.find_by_id(
+        related_entry_id
+    )
+
+    assert isinstance(found_related_entry, ClientCreditEntry)
+
+
+def test_related_entry_invalid(
+    sqlalchemy_client_credits_entries_repo: SQLAlchemyClientCreditEntriesRepository,
+    sqlalchemy_vip_clients_repo: SQLAlchemyVipClientsRepository,
+    make_vip_client,
+    make_client_credit_entry,
+):
+    vip_client = make_vip_client()
+
+    sqlalchemy_vip_clients_repo.create(vip_client)
+
+    invalid_related_entry = uuid4()
+    entry = make_client_credit_entry(
+        quantity=10,
+        source_type=ClientCreditSourceType.REVERSED_BY_ADMIN,
+        vip_client_id=vip_client.id,
+        related_entry_id=invalid_related_entry,
+    )
+
+    with pytest.raises(IntegrityError):
+        sqlalchemy_client_credits_entries_repo.create(entry)
+
+
+def test_get_balance_empty(
+    sqlalchemy_client_credits_entries_repo: SQLAlchemyClientCreditEntriesRepository,
+    sqlalchemy_vip_clients_repo: SQLAlchemyVipClientsRepository,
+    make_vip_client,
+    make_user,
+    make_client_credit_entry,
+):
+    vip_client = make_vip_client()
+    sqlalchemy_vip_clients_repo.create(vip_client)
+    admin = make_user(is_admin=True)
+
+    entry1 = make_client_credit_entry(
+        quantity=10,
+        source_type=ClientCreditSourceType.INDICATION,
+        vip_client_id=vip_client.id,
+    )
+    sqlalchemy_client_credits_entries_repo.create(entry1)
+
+    entry2 = make_client_credit_entry(
+        quantity=10,
+        source_type=ClientCreditSourceType.REVERSED_BY_ADMIN,
+        source_id=admin.id,
+        vip_client_id=vip_client.id,
+        related_entry_id=entry1.id,
+    )
+    sqlalchemy_client_credits_entries_repo.create(entry2)
+
+    balance = sqlalchemy_client_credits_entries_repo.get_balance(vip_client_id=uuid4())
+    assert balance == 0
 
 
 def test_get_correct_balance_from_entries(
@@ -174,6 +298,186 @@ def test_find_many_by_vip_cient_id(
     assert result[0].vip_client_id == vip_client.id
 
 
+def test_count_by_vip_cient_id(
+    sqlalchemy_client_credits_entries_repo: SQLAlchemyClientCreditEntriesRepository,
+    sqlalchemy_vip_clients_repo: SQLAlchemyVipClientsRepository,
+    make_user,
+    make_vip_client,
+    make_client_credit_entry,
+):
+    vip_client = make_vip_client()
+    sqlalchemy_vip_clients_repo.create(vip_client)
+    admin = make_user(is_admin=True)
+
+    entry1 = make_client_credit_entry(
+        quantity=10,
+        source_type=ClientCreditSourceType.INDICATION,
+        vip_client_id=vip_client.id,
+    )
+    sqlalchemy_client_credits_entries_repo.create(entry1)
+
+    entry2 = make_client_credit_entry(
+        quantity=10,
+        source_type=ClientCreditSourceType.ADDED_BY_ADMIN,
+        source_id=admin.id,
+        vip_client_id=vip_client.id,
+    )
+    sqlalchemy_client_credits_entries_repo.create(entry2)
+
+    entry3 = make_client_credit_entry(
+        quantity=10,
+        source_type=ClientCreditSourceType.REVERSED_BY_ADMIN,
+        source_id=admin.id,
+        vip_client_id=vip_client.id,
+        related_entry_id=entry2.id,
+    )
+    sqlalchemy_client_credits_entries_repo.create(entry3)
+
+    entry4 = make_client_credit_entry(
+        quantity=5,
+        source_type=ClientCreditSourceType.INDICATION,
+        vip_client_id=vip_client.id,
+    )
+    sqlalchemy_client_credits_entries_repo.create(entry4)
+
+    entry5 = make_client_credit_entry(
+        quantity=10,
+        source_type=ClientCreditSourceType.USED_IN_APPOINTMENT,
+        source_id=uuid4(),  # appointment id
+        vip_client_id=vip_client.id,
+    )
+    sqlalchemy_client_credits_entries_repo.create(entry5)
+
+    result = sqlalchemy_client_credits_entries_repo.count_by_vip_client_id(
+        vip_client_id=vip_client.id
+    )
+
+    assert result == 5
+    assert isinstance(result, int)
+
+
+def test_pagination_and_direction(
+    sqlalchemy_client_credits_entries_repo: SQLAlchemyClientCreditEntriesRepository,
+    sqlalchemy_vip_clients_repo: SQLAlchemyVipClientsRepository,
+    make_user,
+    make_vip_client,
+    make_client_credit_entry,
+):
+    vip_client = make_vip_client()
+    sqlalchemy_vip_clients_repo.create(vip_client)
+    admin = make_user(is_admin=True)
+
+    entry1 = make_client_credit_entry(
+        quantity=1,
+        source_type=ClientCreditSourceType.INDICATION,
+        vip_client_id=vip_client.id,
+    )
+    sqlalchemy_client_credits_entries_repo.create(entry1)
+
+    entry2 = make_client_credit_entry(
+        quantity=2,
+        source_type=ClientCreditSourceType.ADDED_BY_ADMIN,
+        source_id=admin.id,
+        vip_client_id=vip_client.id,
+    )
+    sqlalchemy_client_credits_entries_repo.create(entry2)
+
+    entry3 = make_client_credit_entry(
+        quantity=3,
+        source_type=ClientCreditSourceType.REVERSED_BY_ADMIN,
+        source_id=admin.id,
+        vip_client_id=vip_client.id,
+        related_entry_id=entry2.id,
+    )
+    sqlalchemy_client_credits_entries_repo.create(entry3)
+
+    entry4 = make_client_credit_entry(
+        quantity=4,
+        source_type=ClientCreditSourceType.INDICATION,
+        vip_client_id=vip_client.id,
+    )
+    sqlalchemy_client_credits_entries_repo.create(entry4)
+
+    entry5 = make_client_credit_entry(
+        quantity=5,
+        source_type=ClientCreditSourceType.USED_IN_APPOINTMENT,
+        source_id=uuid4(),  # appointment id
+        vip_client_id=vip_client.id,
+    )
+    sqlalchemy_client_credits_entries_repo.create(entry5)
+
+    result = sqlalchemy_client_credits_entries_repo.find_many_by_vip_client_id(
+        vip_client_id=vip_client.id, limit=2, offset=1, direction=Direction.asc
+    )
+
+    assert len(result) == 2
+
+    created_ats = [entry.created_at for entry in result]
+
+    assert created_ats == sorted(created_ats)
+
+    assert result[0].quantity == 2
+    assert result[1].quantity == -3  # negative because is reversed by admin
+    # direction asc put oldest entry first and offset 1 take first result out result = entry2 and entry3
+
+
+def test_offset_over_total(
+    sqlalchemy_client_credits_entries_repo: SQLAlchemyClientCreditEntriesRepository,
+    sqlalchemy_vip_clients_repo: SQLAlchemyVipClientsRepository,
+    make_user,
+    make_vip_client,
+    make_client_credit_entry,
+):
+    vip_client = make_vip_client()
+    sqlalchemy_vip_clients_repo.create(vip_client)
+    admin = make_user(is_admin=True)
+
+    entry1 = make_client_credit_entry(
+        quantity=1,
+        source_type=ClientCreditSourceType.INDICATION,
+        vip_client_id=vip_client.id,
+    )
+    sqlalchemy_client_credits_entries_repo.create(entry1)
+
+    entry2 = make_client_credit_entry(
+        quantity=2,
+        source_type=ClientCreditSourceType.ADDED_BY_ADMIN,
+        source_id=admin.id,
+        vip_client_id=vip_client.id,
+    )
+    sqlalchemy_client_credits_entries_repo.create(entry2)
+
+    entry3 = make_client_credit_entry(
+        quantity=3,
+        source_type=ClientCreditSourceType.REVERSED_BY_ADMIN,
+        source_id=admin.id,
+        vip_client_id=vip_client.id,
+        related_entry_id=entry2.id,
+    )
+    sqlalchemy_client_credits_entries_repo.create(entry3)
+
+    entry4 = make_client_credit_entry(
+        quantity=4,
+        source_type=ClientCreditSourceType.INDICATION,
+        vip_client_id=vip_client.id,
+    )
+    sqlalchemy_client_credits_entries_repo.create(entry4)
+
+    entry5 = make_client_credit_entry(
+        quantity=5,
+        source_type=ClientCreditSourceType.USED_IN_APPOINTMENT,
+        source_id=uuid4(),  # appointment id
+        vip_client_id=vip_client.id,
+    )
+    sqlalchemy_client_credits_entries_repo.create(entry5)
+
+    result = sqlalchemy_client_credits_entries_repo.find_many_by_vip_client_id(
+        vip_client_id=vip_client.id, limit=2, offset=9999, direction=Direction.asc
+    )
+
+    assert result == []
+
+
 def test_find_many_by_source_id(
     sqlalchemy_client_credits_entries_repo: SQLAlchemyClientCreditEntriesRepository,
     sqlalchemy_vip_clients_repo: SQLAlchemyVipClientsRepository,
@@ -236,7 +540,7 @@ def test_find_many_by_source_id(
     assert len(result) == 2
 
 
-def test_find_many_by_source_type_and_vip_client_id(
+def test_combined_queries(
     sqlalchemy_client_credits_entries_repo: SQLAlchemyClientCreditEntriesRepository,
     sqlalchemy_vip_clients_repo: SQLAlchemyVipClientsRepository,
     make_user,
