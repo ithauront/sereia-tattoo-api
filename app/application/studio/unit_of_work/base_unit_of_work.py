17dd8ffd8dcf65ec03032cd76f441c7385ff1e25
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-05-02 09:02:05 +0200

    feat[application/studio/unit_of_work]: added repositories in uow
    payments repo
    appointments repo
    audit_log repo

--- app/application/studio/unit_of_work/base_unit_of_work.py
+++ app/application/studio/unit_of_work/base_unit_of_work.py
@@ -1,8 +1,13 @@
 from abc import ABC
 
+from app.application.studio.repositories.appointments_repository import (
+    AppointmentsRepository,
+)
+from app.application.studio.repositories.audit_logs_repository import AuditLogsRepository
 from app.application.studio.repositories.client_credit_entries_repository import (
     ClientCreditEntriesRepository,
 )
+from app.application.studio.repositories.payments_repository import PaymentsRepository
 from app.application.studio.repositories.users_repository import UsersRepository
 from app.application.studio.repositories.vip_clients_repository import (
     VipClientsRepository,
@@ -13,3 +18,6 @@ class BaseUnitOfWork(ABC):
     users: UsersRepository
     vip_clients: VipClientsRepository
     client_credit_entries: ClientCreditEntriesRepository
+    payments: PaymentsRepository
+    appointments: AppointmentsRepository
+    audit_logs: AuditLogsRepository
