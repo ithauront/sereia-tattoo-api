2e5d1c250117d59f46abaf795d7dfc859dc04526
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-06-22 10:24:19 +0200

    feat[application/event_bus/integration_event_bus]: update integration event bus to be able to recive read unit of work and none has default

--- app/application/event_bus/integration_event_bus.py
+++ app/application/event_bus/integration_event_bus.py
@@ -1,11 +1,18 @@
 import asyncio
 
 from app.application.event_bus.event_bus import EventBus
+from app.application.studio.unit_of_work.read_unit_of_work import ReadUnitOfWork
 
 
 class IntegrationEventBus(EventBus):
-    async def publish(self, event):
+    async def publish(self, event, *, uow: ReadUnitOfWork | None = None):
         handlers = self._handlers[type(event)]
 
         for handler in handlers:
-            asyncio.create_task(handler.handle(event))
+            asyncio.create_task(self._safe_call(handler, event, uow))
+
+    async def _safe_call(self, handler, event, uow):
+        try:
+            await handler.handle(event, uow=uow)
+        except TypeError:
+            await handler.handle(event)
--- app/application/event_bus/transactional_event_bus.py
+++ app/application/event_bus/transactional_event_bus.py
@@ -1,10 +1,11 @@
 import asyncio
 
 from app.application.event_bus.event_bus import EventBus
+from app.application.studio.unit_of_work.write_unit_of_work import WriteUnitOfWork
 
 
 class TransactionalEventBus(EventBus):
-    async def publish(self, event, *, uow):
+    async def publish(self, event, *, uow: WriteUnitOfWork):
         handlers = self._handlers[type(event)]
 
         await asyncio.gather(*(handler.handle(event, uow=uow) for handler in handlers))
