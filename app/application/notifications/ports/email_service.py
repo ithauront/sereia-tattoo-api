b28317912c4568ea5af67ac8487a236a411afccc
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-06-28 12:47:43 +0200

    refactor[application/notifications/ports/email_service]: refactor send mail function to async and remove awaitable of return

--- app/application/notifications/ports/email_service.py
+++ app/application/notifications/ports/email_service.py
@@ -1,9 +1,6 @@
 from abc import ABC, abstractmethod
-from typing import Awaitable
 
 
 class EmailService(ABC):
     @abstractmethod
-    def send_email(
-        self, to: str, subject: str, html_content: str
-    ) -> Awaitable[None]: ...
+    async def send_email(self, to: str, subject: str, html_content: str) -> None: ...
