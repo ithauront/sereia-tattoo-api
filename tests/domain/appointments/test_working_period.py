a13a3e40fbb2434880c0b656b83b19d77b5d5a76
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-06-29 12:22:09 +0200

    feat[test/domain/appointments]: test entity working period

---
+++ tests/domain/appointments/test_working_period.py
@@ -0,0 +1,52 @@
+from datetime import time
+
+import pytest
+
+from app.core.exceptions.calendar import (
+    InvalidWeekdayError,
+    WorkingPeriodMustHaveRealisticTimeAndDateError,
+)
+from app.domain.studio.appointments.entities.working_period import WorkingPeriod
+
+
+def test_invalid_weekday_over_6():
+    with pytest.raises(InvalidWeekdayError):
+        WorkingPeriod.create(weekday=7, start_at=time(12, 00), end_at=time(13, 00))
+
+
+def test_invalid_weekday_less_than_0():
+    with pytest.raises(InvalidWeekdayError):
+        WorkingPeriod.create(weekday=-1, start_at=time(12, 00), end_at=time(13, 00))
+
+
+def test_end_before_start():
+    with pytest.raises(WorkingPeriodMustHaveRealisticTimeAndDateError):
+        WorkingPeriod.create(weekday=3, start_at=time(12, 00), end_at=time(10, 00))
+
+
+def test_end_equals_start():
+    with pytest.raises(WorkingPeriodMustHaveRealisticTimeAndDateError):
+        WorkingPeriod.create(weekday=3, start_at=time(12, 00), end_at=time(12, 00))
+
+
+def test_update_with_unrealistic_time_frame():
+    working_period = WorkingPeriod.create(weekday=3, start_at=time(12, 00), end_at=time(14, 00))
+
+    with pytest.raises(WorkingPeriodMustHaveRealisticTimeAndDateError):
+        working_period.update_period(start_at=time(12, 00), end_at=time(12, 00))
+
+
+def test_is_avaiable_for_true():
+    working_period = WorkingPeriod.create(weekday=3, start_at=time(12, 00), end_at=time(14, 00))
+
+    available = working_period.is_available_for(start=time(18, 00), end=time(20, 00))
+
+    assert available is True
+
+
+def test_is_avaiable_for_false():
+    working_period = WorkingPeriod.create(weekday=3, start_at=time(12, 00), end_at=time(14, 00))
+
+    available = working_period.is_available_for(start=time(13, 00), end=time(14, 30))
+
+    assert available is False
