93db01f07718b8ecf4c7be83a7552302cf987b98
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-08-24 14:04:52 +0200

    feat(errors): add serval domain errors for payments flows

--- app/core/exceptions/payment.py
+++ app/core/exceptions/payment.py
@@ -12,3 +12,43 @@ class DuplicateExternalReferenceError(Exception):
 
 class PaymentWithoutAppointmentRequireDescriptionError(Exception):
     pass
+
+
+class PaymentLinkToAppointmentIsCorruptedError(Exception):
+    pass
+
+
+class PaymentMustHaveDepositPurposeError(Exception):
+    pass
+
+
+class PaymentOfThisPurposeMustHaveAppointmentError(Exception):
+    pass
+
+
+class VipClientHasInsufficientCreditError(Exception):
+    pass
+
+
+class PaymentOfThisTypeDoesNotNeedAVipClient(Exception):
+    pass
+
+
+class IdempotencyKeyConflictError(Exception):
+    pass
+
+
+class PaymentAmountExceedsMaximumError(Exception):
+    pass
+
+
+class InvalidPaymentAmountError(Exception):
+    pass
+
+
+class PaymentAmountHasSubCentPrecisionError(Exception):
+    pass
+
+
+class PaymentConsumesZeroClientCreditsError(Exception):
+    pass
